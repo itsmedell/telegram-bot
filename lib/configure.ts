15d0982a53602ee8ff4756d7ec9b360bb6071a67
Lendra <lendev0406@gmail.com>
2021-08-08 20:01:17 +0700

    add configuration tools for lazy people

--- lib/configure.ts
+++ lib/configure.ts
@@ -1,6 +1,40 @@
 import {
     prompt
 } from 'enquirer'
+import fs from 'fs'
 import {
-    configure
+    configFormat,
+    configure,
+    locFiles
 } from './constant'
+import { color } from './utility'
+
+
+prompt(configure)
+.then(async (data: configFormat) => {
+    switch(data.prefix) {
+        case "multi":
+            console.log(color("[System]", "yellow"), color("Gunakan tanda [,] untuk memisahkan antara prefix", "white"))
+            await prompt({
+                type: "input",
+                name: "prefix",
+                message: "Masukan prefix"
+            }).then((res: any) => {
+                data.prefix = res.prefix.split(",")
+            })
+        break
+        case "single":
+            await prompt({
+                type: "input",
+                name: "prefix",
+                message: "Masukan prefix"
+            }).then((res: any) => {
+                data.prefix = res.prefix
+            })
+        break
+        default:
+            return
+    }
+    fs.writeFileSync(locFiles.config, JSON.stringify(data, null, 2))
+    console.log(color('[System]', 'yellow'), color("Konfigurasi sudah disimpan", "green"))
+})
