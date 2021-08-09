e6350a63809a381498a3c8ed7b02d7301343a0a3
Lendra <lendev0406@gmail.com>
2021-08-09 21:34:52 +0700

    plugin/apakah: create

---
+++ plugin/apakah.ts
@@ -0,0 +1,11 @@
+import { Context } from "telegraf";
+import { ContextMessage } from "../lib/constant";
+
+export = {
+    name: 'apakah',
+    aliases: ['is'],
+    description: "Features apakah",
+    execute(ctx: Context, message: ContextMessage) {
+        
+    }
+}
