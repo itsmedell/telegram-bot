32cbb2d4033bf2fbd2b388c9e42561813030ad4a
Lendra <lendev0406@gmail.com>
2021-08-11 06:43:56 +0700

    moving eightball from plugin to message.ts

--- handler/message.ts
+++ handler/message.ts
@@ -29,6 +29,7 @@ export async function MessageHandler(context: Context, rawMessage: any) {
     const body = (text) ? text : (video && caption) ? caption : (photo && caption) ? caption : (audio && caption) ? caption : (document && caption) ? caption : ''
     const commandsName = body.slice(1).trim().split(/ +/).shift().toLowerCase()
     const args = body.trim().split(/ +/).slice(1)
+    const q = args.join(" ")
     const pluginCall = plugins.get(commandsName) || plugins.find(cmd => cmd.aliases && cmd.aliases.includes(commandsName))
     
     // Command Handler
@@ -36,16 +37,36 @@ export async function MessageHandler(context: Context, rawMessage: any) {
         pluginCall.execute(context, message, args)
     } else {
         switch(commandsName) {
+            // Greetings Features
             case "hello":
                 context.reply("Hello Worlds", {
                     reply_to_message_id: message.message_id
                 })
             break
+
+            // Utility Features
             case "ping":
                 context.reply("Pong!", {
                     reply_to_message_id: message.message_id
                 })
             break
+
+            // Games Features
+            case "is":
+            case "eightball":
+            case "8ball":
+                if (!q) return context.reply("Please send your question", {
+                    reply_to_message_id: message.message_id
+                })
+                const listAnswer = ["no", "yes", "i don't know", "maybe", "maybe not", "never", "i don't think so"]
+                const randomAnswer = listAnswer[Math.floor(Math.random() * listAnswer.length)]
+                const formatMessage = `*🎱 Features*\n\n*Question*: ${q}\n*Answer*: ${randomAnswer}`
+                context.reply(formatMessage, {
+                    reply_to_message_id: message.message_id,
+                    parse_mode: 'Markdown'
+                })
+            break
+
             default:
                 if (isCmd(body, config.prefix)) {
                     context.reply("Command not found!", {
