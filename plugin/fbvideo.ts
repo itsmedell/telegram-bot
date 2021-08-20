6e27547559647563112ce62dd18b21cbb93f6cbf
Lendra <lendev0406@gmail.com>
2021-08-20 13:34:50 +0700

    plugin/fbvideo:

--- plugin/fbvideo.ts
+++ plugin/fbvideo.ts
@@ -24,6 +24,7 @@ export = {
                 type: "Video",
                 viewCount: viewCount
             })
+            
             ctx.replyWithPhoto(thumbnail, {
                 reply_to_message_id: message.message_id,
                 caption: resultMessage,
