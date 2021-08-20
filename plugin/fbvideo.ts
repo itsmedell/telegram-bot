13106f1d0a6b6c98bc22174a317e1a591ef783de
Lendra <lendev0406@gmail.com>
2021-08-20 13:35:52 +0700

    add error handler

--- plugin/fbvideo.ts
+++ plugin/fbvideo.ts
@@ -12,24 +12,29 @@ export = {
     async execute(ctx: Context, message: ContextMessage, args: string[]) {
         const fburl = args.length !== 0 ? args[0] : ''
         if (fbdl.getValidUrl(fburl)) {
-            const { title, duration, author, size, uploadDate, linkdl, quality, viewCount, thumbnail } = await fbdl.getVideoInfo(fburl)
-            const resultMessage = msg.fbResult({
-                title: title,
-                duration: duration,
-                author: author,
-                size: size,
-                uploadDate: uploadDate,
-                linkdl: linkdl,
-                quality: quality,
-                type: "Video",
-                viewCount: viewCount
-            })
-            
-            ctx.replyWithPhoto(thumbnail, {
-                reply_to_message_id: message.message_id,
-                caption: resultMessage,
-                parse_mode: "Markdown"
-            })
+            try {
+                const { title, duration, author, size, uploadDate, linkdl, quality, viewCount, thumbnail } = await fbdl.getVideoInfo(fburl)
+                const resultMessage = msg.fbResult({
+                    title: title,
+                    duration: duration,
+                    author: author,
+                    size: size,
+                    uploadDate: uploadDate,
+                    linkdl: linkdl,
+                    quality: quality,
+                    type: "Video",
+                    viewCount: viewCount
+                })
+                ctx.replyWithPhoto(thumbnail, {
+                    reply_to_message_id: message.message_id,
+                    caption: resultMessage,
+                    parse_mode: "Markdown"
+                })
+            } catch (error) {
+                ctx.reply(msg.downloaderError("video"), {
+                    reply_to_message_id: message.message_id
+                })
+            }
         } else {
             ctx.reply(msg.wrongUrl("facebook"), {
                 reply_to_message_id: message.message_id
