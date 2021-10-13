73702ad9efc6f793c3debf7a90512c9af859b344
Dellas <87001693+itsmedell@users.noreply.github.com>
2021-10-13 17:56:33 +0800

    Create bot.ts

--- bot.ts
+++ bot.ts
@@ -61,8 +61,8 @@ export async function starts() {
     })
 
     // Log Info Script
-    console.log(color('[TeleBot]', 'cyan'), color('Now bot is online', 'yellow'))
-    console.log(color('[Developer]', 'cyan'), color("I hope you like this script :)", 'yellow'))
+    console.log(color('[TeleBot]', 'cyan'), color('Now bot is online!', 'yellow'))
+    console.log(color('[DeveloperBot]', 'cyan'), color("I hope you like this script :)", 'yellow'))
 
     // Handler
     handler.EventsHandler(bot)
